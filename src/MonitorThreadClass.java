db206de16934c16f246209f2e638c70206ce5a62
CodechefVaibhav <vaibhav.kashyap@hotmail.com>
2013-09-22 22:36:07 +0530

    letslearnjava.quora.com/Thread-Pool-Concept-in-Java
    Thread Pool i.e. for the first time your thread will be generated as
    fresh & when it will be done with its task and it will be made to store
    into the pool and whenever it will be required in future directly fetch
    live thread from the pool (if available), assign a task & again this
    cycle continues. You don't need to start a thread after initial start
    you made it for first time.

---
+++ src/MonitorThreadClass.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author vaibhav
+ */
+public class MonitorThreadClass implements Runnable {
+    
+    public static boolean firstExecute;
+    int j =0;
+    @Override
+    public void run()
+    {
+        while(true)
+        {
+            if(!firstExecute)
+            {
+                MyThreadProcessor mtpObject = MyThreadPool.fetchFreeThread();
+                if(mtpObject == null)
+                { 
+                    try
+                    {
+                        System.out.println("No Free Thread Available , Try after some time");
+                        Thread.sleep(5000);
+                    }
+                    catch(Exception e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+
+                if(mtpObject!=null)mtpObject.performOperation();
+            }
+            else
+            {
+                MyThreadPool.initialcall();
+            }
+        }
+    }
+}
---
+++ src/MyMainClass.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author vaibhav
+ */
+public class MyMainClass {
+
+    Thread monitor = new Thread(new MonitorThreadClass());
+    MyThreadPool mp = new MyThreadPool();
+    public static void main(String...q)
+    {
+       new MyMainClass().startMonitorThread(); 
+    }
+    
+    public void startMonitorThread()
+    {
+       mp.init();
+       monitor.start();
+    }
+}
